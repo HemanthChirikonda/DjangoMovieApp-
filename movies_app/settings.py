b727fd3e355cc943ba8479c520662231a19fa211
hemanthChirikonda <hemanthchirikonda@gmail.com>
2021-05-24 02:00:59 +0530

    Added files and changes required for deployment to heroku

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -137,15 +137,15 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# # Simplified static file serving.
-# # https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/3.1/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# # The absolute path to the directory where collectstatic will collect static files for deployment.
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# # The URL to use when referring to static files (where they will be served from)
-# STATIC_URL = '/static/'
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
