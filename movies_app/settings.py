41b542860277744f92094d11ee5ef269d8255f05
hemanthChirikonda <hemanthChirikonda@gmail.com>
2021-05-23 23:42:14 +0530

    env addition

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p*i%xcznb55(9ud+77=-nm$b!p9kmbkc&h@8n2g8psb$@5ta%i'
+SECRET_KEY = env('KEY_CODE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
