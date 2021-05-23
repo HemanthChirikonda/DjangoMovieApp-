342fa01e0745977cee16eaa3133ee51099774b1b
hemanthChirikonda <hemanthChirikonda@gmail.com>
2021-05-23 18:42:18 +0530

    edit func add

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
-from .views import home_page, create
+from .views import edit, home_page, create
 from django.urls import path
 
 
 urlpatterns=[
     path('', home_page, name="home_page" ),
-    path('create/', create, name='create')
+    path('create/', create, name='create'),
+    path('edit/<str:movie_id>',edit,name='edit')
 ]
