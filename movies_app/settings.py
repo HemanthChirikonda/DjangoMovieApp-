b2a9f1306a4bdc3af508a2b65ebf2fe01351e182
hemanthChirikonda <hemanthchirikonda@gmail.com>
2021-05-25 00:43:36 +0530

    host update

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('KEY_CODE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','sleepy-plateau-79366.herokuapp.com']
 
 
 # Application definition
