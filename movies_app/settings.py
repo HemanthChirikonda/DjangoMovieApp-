8bc1e1dc0990b142a74b4100059d515553198af8
hemanthChirikonda <hemanthchirikonda@gmail.com>
2021-05-24 12:01:03 +0530

    fix 4

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -133,9 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
