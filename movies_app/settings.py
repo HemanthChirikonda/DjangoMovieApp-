ea868138f3c629e6aa595c3b227c6a282a272c42
hemanthChirikonda <hemanthchirikonda@gmail.com>
2021-05-24 16:19:25 +0530

    fix 2

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('KEY_CODE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sleepy-plateau-79366.herokuapp.com','127.0.0.1']
 
 
 # Application definition
